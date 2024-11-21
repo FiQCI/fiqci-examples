55b3bb7bef49b85e8bbd6877684d3f04c0428fb0
Modupe <falodunmodupeola@gmail.com>
2024-11-21 15:41:59 +0200

    Removed unnecessary files for this branch

--- scripts/int_job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Launch this script as
-# > bash int_job.sh 'qb_flip_qiskit.py --backend helmi'
-
-clear
-srun --account project_xxx -t 00:15:00 -c 1 -n 1 --partition q_fiqci python -u $1
