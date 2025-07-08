a4275fc875c1c91176355c9be988e546ab8765e8
Joonas Nivala <joonas.nivala@gmail.com>
2025-07-08 11:35:31 +0300

    update bash scripts

--- scripts/batch_script.sh
+++ scripts/batch_script.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=helmijob   # Job name
-#SBATCH --output=helmijob.o%j # Name of stdout output file
-#SBATCH --error=helmijob.e%j  # Name of stderr error file
+#SBATCH --job-name=qcjob   # Job name
+#SBATCH --output=out.txt # Name of stdout output file
+#SBATCH --error=err.txt  # Name of stderr error file
 #SBATCH --partition=q_fiqci   # Partition (queue) name
 #SBATCH --ntasks=1            # One task (process)
 #SBATCH --cpus-per-task=1     # Number of cores (threads)
 #SBATCH --time=00:15:00       # Run time (hh:mm:ss)
-#SBATCH --account=project_xxx # Project for billing
+#SBATCH --account=project_xxxxxxx # Project for billing
 #SBATCH --mem-per-cpu=1G      # Memory per CPU
 
 module use /appl/local/quantum/modulefiles
-module load helmi_qiskit  # Load the module to use qiskit on Helmi
+module load fiqci-vtt-qiskit  # Load the module to use qiskit on Helmi
 
-# module load helmi_cirq    # Load the module to use cirq on Helmi
+# module load fiqci-vtt-cirq    # Load the module to use cirq on Helmi
 
-# Save the job ID to a file for later reference
-echo $SLURM_JOB_ID >> job_id.txt
+export DEVICES=("Q5") # for Q50 use export DEVICES=("Q50") 
+                      # for both Q5 and Q50 use export DEVICES=("Q5" "Q50")
 
-python -u $1
+source $RUN_SETUP
+
+python -u $1 # Usage: sbatch batch_script.sh <your_python_script.py>
