00dd59df7c4cc6ee2c50f5108a063af73931815d
Jake Muff <jake.muff@vtt.fi>
2026-01-07 18:21:29 +0200

    Update get calibration data python script
    Signed-off-by: Jake Muff <jake.muff@vtt.fi>

--- scripts/get_calibration_data.py
+++ scripts/get_calibration_data.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import requests
 from iqm.iqm_client import IQMClient  # Requires iqm_client==15.3
@@ -12,21 +11,18 @@ def get_calibration_data(client: IQMClient, calibration_set_id=None, filename: s
     Optionally you can input a calibration set id (UUID) to query historical results
     Optionally save the response to a json file, if filename is provided
     """
-    headers = {'User-Agent': client._signature}
-    bearer_token = client._token_manager.get_bearer_token()
-    headers['Authorization'] = bearer_token
+    headers = {"User-Agent": client._iqm_server_client._signature}
+    bearer_token = client._iqm_server_client._auth_header_callback()
+    headers["Authorization"] = bearer_token
 
-    url = os.path.join(
-        client._api.iqm_server_url,
-        'calibration/metrics/latest',
-    )
+    server_client = client._iqm_server_client
+    root_url = server_client.root_url
+    quantum_computer = server_client.quantum_computer
+
     if calibration_set_id:
-        url = os.path.join(url, calibration_set_id)
+        url = f"{root_url}/api/devices/{quantum_computer}/calibration/metrics/{calibration_set_id}"
     else:
-        url = os.path.join(
-            client._api.iqm_server_url,
-            'calibration/metrics/latest',
-        )
+        url = f"{root_url}/api/devices/{quantum_computer}/calibration/metrics/latest"
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()  # will raise an HTTPError if the response was not ok
@@ -35,22 +31,18 @@ def get_calibration_data(client: IQMClient, calibration_set_id=None, filename: s
     data_str = json.dumps(data, indent=4)
 
     if filename:
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             f.write(data_str)
         print(f"Data saved to {filename}")
 
     return data
 
 
-HELMI_CORTEX_URL = os.getenv('HELMI_CORTEX_URL')
-if not HELMI_CORTEX_URL:
-    raise ValueError(
-        'Environment variable HELMI_CORTEX_URL is not set. Are you running on Lumi and on the q_fiqci node?',
-    )
-
 # Using Qiskit as an example of how to query using this function.
 
-provider = IQMProvider(HELMI_CORTEX_URL)
+iqm_server_url = "https://<IQM SERVER>"  # Replace this with the correct URL
+quantum_computer = "<NAME>"
+provider = IQMProvider(iqm_server_url, quantum_computer=quantum_computer)
 backend = provider.get_backend()
 
 calibration_data = get_calibration_data(backend.client)
